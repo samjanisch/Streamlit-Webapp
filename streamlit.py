d4cfc394cb38d13f01591290c1e9a36fca9d015e
Sam Janisch <sjanisch77@gmail.com>
2021-03-09 23:00:18 -0800

    Added chart

--- streamlit.py
+++ streamlit.py
@@ -1,3 +1,19 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+
+st.title('title text goes here')
+
+df = pd.DataFrame({
+    'first column' : [1, 4, 7],
+    'second column' : [12, 6, 12]
+})
+
+df
+
+chart_data = pd.DataFrame(
+    np.random.randn(60,3),
+    columns=[1,2,3]
+)
+
+chart_data
